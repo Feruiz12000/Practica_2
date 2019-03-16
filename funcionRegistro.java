1f4a558c1323fc69393fb232a42cfe884b41eb62
Jefferson Linares - 201504448 <diegoforlan102010@hotmail.com>
2019-03-16 14:40:41 -0600

    funcion crear playlist - 201504448 0.1.1

--- funcionRegistro.java
+++ funcionRegistro.java
@@ -6,3 +6,21 @@ public bool registrarNuevoUsuario (Usuario user){
     return true;
 }
 
+
+/*
+*@param user contiene toda la informacion del usuario
+*@param playlist contiene toda la informacion de la playlist creada
+	(nombre, descripcion, artista);
+
+
+*/
+
+public void crearPlayList(Usuario user, PlayList playlist){
+	//variable user contiene los datos del usuario
+	user.addPlaylist(playlist);//añadir playlist al usuario
+
+	//actualizamos la base de datos agregando la nueva playlist creada al usuario
+	mysql.procedure_addPlaylist(user.getPlaylist.last);
+
+}
+
