358170ebe8f2948667fc7e2f8f310b1d967b10ca
Jefferson Linares - 201504448 <diegoforlan102010@hotmail.com>
2019-03-17 23:32:10 -0600

    Funcionalidad reproduccion aleatoria - 201504448 0.2.4

--- FuncionalidadAdicional.java
+++ FuncionalidadAdicional.java
@@ -3,7 +3,12 @@ public bool Reproducir(Usuario user, Cancion cancion){
     //ir a la pantalla Reproductor
     //seleccionar cancion
     //ver opciones
-	//clic en reproducir cancion
+	//clic en reproducir cancion 
+
+//---------reproduccion en modo aleatorio----------
+	cancion.play(Random);
 	
     return true;
 }
+
+
